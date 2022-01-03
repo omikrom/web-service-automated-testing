41128184b76f9e0d1fd0fd479920f1afa2c2d389
Richard Lamnea <omikrom2@gmail.com>
2022-01-03 20:46:17 +0000

    commit

--- service.js
+++ service.js
@@ -118,7 +118,8 @@ service.patch('/user/:id', async (req, res) => {
 });	
 
 //get user by name ?name=rich
-service.get('/user', async (req, res) => {                                            
+service.get('/user', async (req, res) => { 
+	console.log(req.query.name);                                           
 	if(typeof req.query.username === "string")
 	{
 		await userModel.SelectUserByName(req.query.username, res);
